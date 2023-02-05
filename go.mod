80e6cbed4ff33bf218be363032102e434911e9ab
Noah Huppert <contact@noahh.io>
2023-02-04 20:29:17 -0500

    wip trying wails for gui

--- go.mod
+++ go.mod
@@ -3,14 +3,41 @@ module github.com/Noah-Huppert/qmk-gui
 go 1.19
 
 require (
+	github.com/segmentio/encoding v0.3.4
+	github.com/wailsapp/wails/v2 v2.3.1
+	go.lsp.dev/jsonrpc2 v0.10.0
+	go.lsp.dev/protocol v0.12.0
+	go.lsp.dev/uri v0.3.0
+	go.uber.org/zap v1.24.0
+)
+
+require (
+	github.com/bep/debounce v1.2.1 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
+	github.com/labstack/echo/v4 v4.9.0 // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/leaanthony/go-ansi-parser v1.0.1 // indirect
+	github.com/leaanthony/gosod v1.0.3 // indirect
+	github.com/leaanthony/slicer v1.5.0 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pkg/browser v0.0.0-20210706143420-7d21f8c997e2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/samber/lo v1.27.1 // indirect
 	github.com/segmentio/asm v1.1.3 // indirect
-	github.com/segmentio/encoding v0.3.4 // indirect
-	go.lsp.dev/jsonrpc2 v0.10.0 // indirect
+	github.com/tkrajina/go-reflector v0.5.5 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	github.com/wailsapp/mimetype v1.4.1 // indirect
 	go.lsp.dev/pkg v0.0.0-20210717090340-384b27a52fb2 // indirect
-	go.lsp.dev/protocol v0.12.0 // indirect
-	go.lsp.dev/uri v0.3.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	go.uber.org/zap v1.24.0 // indirect
-	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -15,11 +15,40 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
+
+	"embed"
 
 	"github.com/Noah-Huppert/qmk-gui/clangdlsp"
 	"github.com/Noah-Huppert/qmk-gui/cmd"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+// Greet returns a greeting for the given name
+func (a *App) Greet(name string) string {
+	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
 
 // JSON RPC connection handler.
 type ClientHandler struct {
@@ -151,35 +180,28 @@ func main() {
 	}
 	defer logger.Sync()
 
-	// Start LSP server
-	proc, err := cmd.NewCmdCloser(ctx, logger, "clangd", []string{"--log=verbose", "--limit-results=0"})
+	// Setup GUI
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err = wails.Run(&options.App{
+		Title:  "qmk-gui",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
 	if err != nil {
-		logger.Fatal("failed to run C LSP", zap.Error(err))
+		println("Error:", err.Error())
 	}
-
-	go func() {
-		ticker := time.NewTicker(time.Millisecond * 200)
-
-		line := ""
-
-		for {
-			select {
-			case <-ticker.C:
-				stderrBuff := make([]byte, 2048)
-				n, err := proc.ReadStderr(stderrBuff)
-				if err != nil {
-					logger.Error("failed to read LSP stderr (used for debug msgs usually)", zap.Error(err))
-				}
-
-				if n > 0 {
-					decoded := string(stderrBuff)
-
-					for _, char := range decoded {
-						charStr := string(char)
-
-						if charStr == "\u0000" {
-							continue
-						}
 
 	// Start LSP server
 	proc, err := cmd.NewCmdCloser(ctx, logger, "clangd", []string{
@@ -188,7 +210,7 @@ func main() {
 	})
 	if err != nil {
 		logger.Fatal("failed to run C LSP", zap.Error(err))
-						}
+	}
 
 	logger.Info("running lsp")
 
